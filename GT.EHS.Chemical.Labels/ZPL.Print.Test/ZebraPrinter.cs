7513a1de26593e510b4580a6acef50b367339f66
Rogelio Arriaga <r.arriaga@generaltransmissions.com>
2024-06-18 19:27:32 -0500

    Update Funtional Program Example

---
+++ GT.EHS.Chemical.Labels/ZPL.Print.Test/ZebraPrinter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Sockets;
+
+
+namespace ZPL.Print.Test
+{
+    public class ZebraPrinter
+    {
+        private string _printerIp;
+        private int _printerPort;
+
+        public ZebraPrinter(string printerIp, int printerPort = 9100)
+        {
+            _printerIp = printerIp;
+            _printerPort = printerPort;
+        }
+
+        public void Send(string zplCommands)
+        {
+            using (var tcpClient = new TcpClient())
+            {
+                try
+                {
+                    tcpClient.Connect(_printerIp, _printerPort);
+                    Console.WriteLine("Conexión establecida.");
+
+                    using (var writer = new StreamWriter(tcpClient.GetStream()))
+                    {
+                        writer.Write(zplCommands);
+                        writer.Flush();
+                        Console.WriteLine("Datos enviados a la impresora.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error al enviar la etiqueta: {ex.Message}");
+                }
+                finally
+                {
+                    tcpClient.Close();
+                    Console.WriteLine("Conexión cerrada.");
+                }
+            }
+        }
+    }
+}
---
+++ GT.EHS.Chemical.Labels/ZPL.Print.Test/ZplLabel.cs
@@ -0,0 +1,110 @@
+﻿using System.Text;
+
+namespace ZPL.Print.Test
+{
+    public class ZplLabel
+    {
+        private StringBuilder _zpl;
+
+        public ZplLabel()
+        {
+            _zpl = new StringBuilder();
+            _zpl.Append("^XA"); // Iniciar etiqueta
+        }
+
+        public ZplLabel Plantilla()
+        {
+            _zpl.AppendFormat(@"
+                ^FX Cuadrado de contorno
+                ^FO30,18^GB1180,885,3^FS
+
+                ^FX Rectangulo Negro
+                ^FO50,18
+                ^GB700,130,130^FS
+
+
+                ^FX Texto De Atencion
+                ^FO750,160^CFG
+                ^ADN,50,30
+                ^FD ATENCION ^FS
+
+                ^FX Texto De Rev
+                ^FO1000,880^CFG
+                ^ADN,10,10
+                ^FD ETI-023 Rev. C ^FS
+
+                ^FX Cuadrados de pictogramas
+
+                ^FO50,200
+                ^GB110,110,1^FS
+
+                ^FO55,205
+                
+
+                ^FO50,310
+                ^GB110,110,1^FS
+
+                ^FO55,315
+               
+
+
+                ^FO160,200
+                ^GB110,110,1^FS
+
+                ^FO165,205
+                
+
+                ^FO160,310
+                ^GB110,110,1^FS
+
+                ^FO160,315
+                
+                ");
+            return this;
+        }
+
+        public ZplLabel ChemicalName(string text)
+        {
+            _zpl.AppendFormat($"^LRY^FO60,50^CFG^ADN,50,25^FD{text}^FS");
+            return this;
+        }
+        public ZplLabel ExpirationDate(string dateTime)
+        {
+            _zpl.AppendFormat($"^FO30,830 " +
+                $"^GB700,70,1^FS " +
+                $"^FO35,850^CFG " +
+                $"^ADN,30,10 " +
+                $"^FD Fecha de expiracion: {dateTime}^FS");
+            return this;
+        }
+        #region
+        public ZplLabel AddPictogram_1(string text,int x = 55, int y = 205)
+        {
+            _zpl.AppendFormat("^FO{0},{1},{2}", x, y, text);
+            return this;
+        }
+        public ZplLabel AddPictogram_2(string text, int x = 55, int y = 315)
+        {
+            _zpl.AppendFormat("^FO{0},{1},{2}", x, y, text);
+            return this;
+        }
+        public ZplLabel AddPictogram_3(string text, int x = 165, int y = 205)
+        {
+            _zpl.AppendFormat("^FO{0},{1},{2}", x, y, text);
+            return this;
+        }
+        public ZplLabel AddPictogram_4(string text, int x = 160, int y = 315)
+        {
+            _zpl.AppendFormat("^FO{0},{1},{2}", x, y, text);
+            return this;
+        }
+
+        #endregion
+
+        public string Build()
+        {
+            return _zpl.Append("^XZ").ToString(); // Finalizar etiqueta
+        }
+    }
+
+}
